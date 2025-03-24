a1fead374d1af3a2e2842e79ef56d5007ab931fe
Gaurav <gauravkalmodiya@gmail.com>
2025-03-24 21:20:35 +0530

    footer-fix

--- app/components/Navbar/Contactus.tsx
+++ app/components/Navbar/Contactus.tsx
@@ -1,193 +1,131 @@
-"use client"
+"use client";
 import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
 import Link from 'next/link';
 
-const Contactusform = () => {
-    let [isOpen, setIsOpen] = useState(false)
-
-    const [inputValues, setInputValues] = useState({
-        input1: '',
-        input2: '',
-        input3: '',
-        input4: ''
-    });
-
-    const handleChange = (e: { target: { name: string; value: string; }; }) => {
-        const { name, value } = e.target;
-        setInputValues(prevState => ({ ...prevState, [name]: value }));
-    }
-
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        
-        try {
-            const response = await fetch('https://api.web3forms.com/submit', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    access_key: '18586efc-2896-4d66-98ff-ea546e339a07',
-                    input1: inputValues.input1,
-                    input2: inputValues.input2,
-                    input3: inputValues.input3,
-                    input4: inputValues.input4
-                }),
-            });
-
-            const data = await response.json();
-            
-            if (response.ok) {
-                setInputValues({
-                    input1: '',
-                    input2: '',
-                    input3: '',
-                    input4: ''
-                });
-                setIsOpen(false);
-            } else {
-                alert('There was an error submitting your form. Please try again.');
-                console.error('Form submission failed:', data);
-            }
-        } catch (error) {
-            console.error('Form submission failed:', error);
-            alert('There was an error submitting your form. Please try again.');
-        }
-    };
-
-    // Update validation to only require name, phone, and message
-    const isDisabled = ['input1', 'input3', 'input4'].some(key => {
-        return inputValues[key as keyof typeof inputValues] === '' ||
-               (key === 'input4' && !(inputValues[key as keyof typeof inputValues].length === 10 &&
-               /^\d+$/.test(inputValues[key as keyof typeof inputValues])));
-      });
-      
-    const closeModal = () => {
-        setIsOpen(false)
-    }
-
-    const openModal = () => {
-        setIsOpen(true)
-    }
-
-    return (
-        <>
-            <div className=" inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto md:ml-6 sm:pr-0">
-                <div className='hidden lg:block'>
-                    <button
-                        type="button"
-                        className='justify-end text-xl font-semibold bg-lightblue py-4 px-6 lg:px-12 navbutton rounded-full hover:bg-darkblue hover:text-white'
-                        onClick={openModal}
-                    >
-                        Contact Us
-                    </button>
-                </div>
-            </div>
-
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-50" onClose={closeModal}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-
-                                    <div className="py-8 lg:py-8 px-4 mx-auto max-w-screen-md">
-                                        <div className="flex flex-shrink-0 items-center justify-center">
-                                            <Link href="/" className='text-2xl sm:text-4xl font-semibold text-black'>
-                                                Astro Rajesh Sharma
-                                            </Link>
-                                        </div>
-                                        <p className="mb-8 lg:mb-16 mt-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Contact us now? Want to send us a feedback?</p>
-                                        <form onSubmit={handleSubmit} className="space-y-8">
-                                            <input type="hidden" name="access_key" value="18586efc-2896-4d66-98ff-ea546e339a07" />
-                                            
-                                            <div>
-                                                <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
-                                                <input
-                                                    id="text"
-                                                    name="input1"
-                                                    value={inputValues.input1}
-                                                    onChange={handleChange}
-                                                    type="text"
-                                                    autoComplete="current-password"
-                                                    required
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                                    placeholder="Name..."
-                                                />
-                                            </div>
-                                            <div>
-                                                <label htmlFor="contact" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Phone number</label>
-                                                <input
-                                                    id="phone"
-                                                    name="input4"
-                                                    value={inputValues.input4}
-                                                    onChange={handleChange}
-                                                    type="tel"
-                                                    autoComplete="current-password"
-                                                    minLength={10}
-                                                    maxLength={10}
-                                                    required
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                                    placeholder="Phone number..."
-                                                />
-                                            </div>
-                                            <div>
-                                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email (optional)</label>
-                                                <input
-                                                    id="email"
-                                                    name="input2"
-                                                    value={inputValues.input2}
-                                                    onChange={handleChange}
-                                                    type="email"
-                                                    autoComplete="current-password"
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                                                    placeholder="xyz@email.com"
-                                                />
-                                            </div>
-                                            <div className="sm:col-span-2">
-                                                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                                                <textarea
-                                                    id="message"
-                                                    name="input3"
-                                                    value={inputValues.input3}
-                                                    onChange={handleChange}
-                                                    className="relative block w-full appearance-none  rounded-md border border-linegrey px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Leave a comment..."
-                                                ></textarea>
-                                            </div>
-                                            <button type="submit"
-                                                disabled={isDisabled}
-                                                className="py-3 px-5 text-sm disabled:opacity-50 font-medium w-full text-center text-white rounded-lg bg-blue focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-                                        </form>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </>
-    )
+interface ContactusformProps {
+  isOpen: boolean;
+  closeModal: () => void;
 }
+
+const Contactusform: React.FC<ContactusformProps> = ({ isOpen, closeModal }) => {
+
+  const [inputValues, setInputValues] = useState({
+    input1: '',
+    input2: '',
+    input3: '',
+    input4: ''
+  });
+
+  const handleChange = (e: { target: { name: string; value: string; }; }) => {
+    const { name, value } = e.target;
+    setInputValues(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          access_key: '18586efc-2896-4d66-98ff-ea546e339a07',
+          input1: inputValues.input1,
+          input2: inputValues.input2,
+          input3: inputValues.input3,
+          input4: inputValues.input4
+        }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setInputValues({
+          input1: '',
+          input2: '',
+          input3: '',
+          input4: ''
+        });
+        closeModal();
+      } else {
+        alert('There was an error submitting your form. Please try again.');
+        console.error('Form submission failed:', data);
+      }
+    } catch (error) {
+      console.error('Form submission failed:', error);
+      alert('There was an error submitting your form. Please try again.');
+    }
+  };
+
+  const isDisabled = ['input1', 'input3', 'input4'].some(key => {
+    return inputValues[key as keyof typeof inputValues] === '' ||
+           (key === 'input4' && !(inputValues[key as keyof typeof inputValues].length === 10 &&
+           /^\d+$/.test(inputValues[key as keyof typeof inputValues])));
+  });
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <button onClick={closeModal} className="text-black text-xl mb-4">✖ Close</button>
+                <h3 className="text-center text-2xl font-semibold">Astro Rajesh Sharma</h3>
+                <h3 className="text-center text-2xl font-semibold">Contact Us</h3>
+                
+                <form onSubmit={handleSubmit} className="space-y-8">
+                  <input type="hidden" name="access_key" value="18586efc-2896-4d66-98ff-ea546e339a07" />
+                  
+                  <div>
+                    <label className="block mb-2">Your Name</label>
+                    <input name="input1" value={inputValues.input1} onChange={handleChange} type="text" placeholder="Name" required className="border p-2 w-full"/>
+                  </div>
+                  <div>
+                    <label className="block mb-2">Your Phone Number</label>
+                    <input name="input4" value={inputValues.input4} onChange={handleChange} type="tel" minLength={10} maxLength={10} required className="border p-2 w-full"/>
+                  </div>
+                  <div>
+                    <label className="block mb-2">Your Email (Optional)</label>
+                    <input name="input2" value={inputValues.input2} onChange={handleChange} type="email" placeholder="example@mail.com" className="border p-2 w-full"/>
+                  </div>
+                  <div>
+                    <label className="block mb-2">Your Message</label>
+                    <textarea name="input3" value={inputValues.input3} onChange={handleChange} placeholder="Leave a message" className="border p-2 w-full"/>
+                  </div>
+                  <div className="flex justify-center">
+                    <button type="submit" disabled={isDisabled} className="bg-blue text-white py-2 px-4 rounded disabled:opacity-80">Send Message</button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
 
 export default Contactusform;
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Disclosure } from '@headlessui/react';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Drawer from "./Drawer";
 import Drawerdata from "./Drawerdata";
@@ -9,92 +9,106 @@ import Contactusform from './Contactus';
 import Image from 'next/image';
 
 interface NavigationItem {
-    name: string;
-    href: string;
-    current: boolean;
+  name: string;
+  href: string;
+  current: boolean;
 }
 
 const navigation: NavigationItem[] = [
-    { name: 'Home', href: '#banner', current: false },
-    { name: 'Services', href: '/services', current: false },
-    { name: 'FAQ', href: '#faq-section', current: false },
-    { name: 'Blog', href:  '/blog', current: false },
-    { name: 'Testimonial', href: '#testimonial-section', current: false },
+  { name: 'Home', href: '#banner', current: false },
+  { name: 'Services', href: '/services', current: false },
+  { name: 'FAQ', href: '#faq-section', current: false },
+  { name: 'Blog', href: '/blog', current: false },
+  { name: 'Testimonial', href: '#testimonial-section', current: false },
 ];
 
 function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(' ');
 }
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isContactFormOpen, setContactFormOpen] = useState(false);
 
-    return (
-        <Disclosure as="nav" className="navbar bg-white">
-            {({ open }) => (
-                <>
-                    <div className="mx-auto max-w-100% p-3 md:p-4 lg:px-8">
-                        <div className="relative flex h-12 sm:h-20 items-center justify-between">
-                            {/* Logo Section */}
-                            <div className="flex items-center">
-                                <Link href="#banner">
-                                    {/* <span className="text-2xl sm:text-4xl font-semibold text-gray-900 hover:text-indigo-600 transition-colors">
-                                        Astro Rajesh
-                                    </span> */}
-                                    <Image src="/images/manage/logo.png" width={200} height={200} alt="" className=''/>
- 
-                                </Link>
-                            </div>
+  const handleContactClick = () => {
+    setContactFormOpen(true);
+  };
 
-                            {/* Desktop Navigation */}
-                            <div className="hidden lg:flex lg:items-center lg:space-x-4">
-                                {navigation.map((item) => (
-                                    <Link
-                                        key={item.name}
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current 
-                                                ? 'bg-[#A3C3E1] text-white'  // Set the active link background to a blue color
-                                                : 'text-gray-700 hover:bg-[#90B9E9] hover:text-white',  // Light blue on hover
-                                            'px-3 py-4 rounded-md text-lg font-normal transition-all duration-200'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </Link>
-                                ))}
-                            </div>
+  return (
+    <>
+      <Disclosure as="nav" className="navbar bg-white">
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-100% p-3 md:p-4 lg:px-8">
+              <div className="relative flex h-12 sm:h-20 items-center justify-between">
+                
+                {/* Logo Section */}
+                <div className="flex items-center">
+                  <Link href="#banner">
+                    <Image src="/images/manage/logo.png" width={200} height={200} alt="Astro Rajesh" />
+                  </Link>
+                </div>
 
-                            {/* Contact Button */}
-                            <div className="hidden lg:flex lg:items-center">
-                                <Contactusform />
-                            </div>
+                {/* Desktop Navigation */}
+                <div className="hidden lg:flex lg:items-center lg:space-x-4">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? 'bg-[#A3C3E1] text-white'
+                          : 'text-gray-700 hover:bg-[#90B9E9] hover:text-white',
+                        'px-3 py-4 rounded-md text-lg font-normal transition-all duration-200'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
 
-                            {/* Mobile Menu Button */}
-                            <div className="flex lg:hidden">
-                                <Disclosure.Button
-                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none"
-                                    onClick={() => setIsOpen(!isOpen)}
-                                >
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
-                        </div>
-                    </div>
+                {/* Contact Button */}
+                <div className="hidden lg:flex lg:items-center">
+                  <button
+                    onClick={handleContactClick}
+                    className="text-xl font-semibold bg-lightblue py-4 px-6 lg:px-12 rounded-full hover:bg-darkblue hover:text-white"
+                  >
+                    Contact Us
+                  </button>
+                </div>
 
-                    {/* Mobile Drawer */}
-                    <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
-                        <Drawerdata />
-                    </Drawer>
-                </>
-            )}
-        </Disclosure>
-    );
+                {/* Mobile Menu Button */}
+                <div className="flex lg:hidden">
+                  <Disclosure.Button
+                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none"
+                    onClick={() => setIsOpen(!isOpen)}
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            {/* Mobile Drawer */}
+            <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
+              <Drawerdata />
+            </Drawer>
+          </>
+        )}
+      </Disclosure>
+
+      {/* Contact Form Modal */}
+      {isContactFormOpen && (
+        <Contactusform isOpen={isContactFormOpen} closeModal={() => setContactFormOpen(false)} />
+      )}
+    </>
+  );
 };
 
 export default Navbar;
